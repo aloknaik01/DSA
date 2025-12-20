64565d76138a0a64e40db49a7025023565861de7
Alok Kumar Naik <aloknaik016261@gmail.com>
2025-12-20 09:07:24 +0530

    add solid Diamond pattern

---
+++ Pattern/pattern/diamond/SolidDiamond.java
@@ -0,0 +1,34 @@
+package Pattern.pattern.diamond;
+
+public class SolidDiamond {
+    public static void main() {
+        System.out.println("Enter no of rows");
+        int n = new java.util.Scanner(System.in).nextInt();
+
+        int space =  n/2;
+        int star = 1;
+
+
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=space; j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=1; k<=star; k++)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println();
+            if(i<=n/2) {
+                space--;
+                star+=2;
+            }
+            else {
+                space++;
+                star-=2;
+            }
+        }
+    }
+}
