9d374946382eea231f44607c8c149ab5a147c831
Alok Kumar Naik <aloknaik016261@gmail.com>
2025-12-23 16:46:50 +0530

    add reverse array by swap through loop

--- Arrays/ReverseArray.java
+++ Arrays/ReverseArray.java
@@ -4,9 +4,12 @@ import java.util.Arrays;
 
 public class ReverseArray {
     public static void main() {
-        int arr[] = { 9, 4, 6, 3, 7,4,6,63,0,43,64,73};
+        int arr[] = { 1,2,3,4};
         int Loopres[] = reverseByLoop(arr);
         System.out.println(Arrays.toString(Loopres));
+
+        int swapLoopRes[] = reversebySwapByLoop(arr);
+        System.out.println(Arrays.toString(swapLoopRes));
 
     }
 
@@ -24,4 +27,24 @@ public class ReverseArray {
 
         return b;
     }
+
+    //Reverse Array By Swap through Loop
+    static int[] reversebySwapByLoop(int currentArray[])
+    {
+        int i = 0;
+        int j = currentArray.length - 1;
+
+        while (i < j)
+        {
+         int temp = currentArray[i];
+         currentArray[i] = currentArray[j];
+         currentArray[j] = temp;
+         i++;
+         j--;
+        }
+        return currentArray;
+
+    }
+
+
 }
