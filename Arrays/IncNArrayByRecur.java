e4925fed44c810a3090bd81200fba17dfdb4b909
Alok Kumar Naik <aloknaik016261@gmail.com>
2025-12-23 15:58:29 +0530

    updating array elements in decresing order from 5 to 1

---
+++ Arrays/IncNArrayByRecur.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class IncNArrayByRecur {
+    public static void main() {
+        System.out.println("Enter array size");
+        int n  = new java.util.Scanner(System.in).nextInt();
+        int arr[] = new int[n];
+        int res[] = incNArrayByRecur(arr, 0, n);
+        System.out.println(Arrays.toString(res));
+    }
+
+    static int[] incNArrayByRecur(int a[], int index, int n){
+
+        if(index > a.length-1) return a;
+        a[index] = n;
+        return incNArrayByRecur(a, ++index,--n);
+    }
+}
