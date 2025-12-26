c0361b159de080ceaf9eb182e47a28d0a127bbbb
Alok Kumar Naik <aloknaik016261@gmail.com>
2025-12-26 10:00:21 +0530

    add Bubble Sort

---
+++ Arrays/Sorting/BubbleSort.java
@@ -0,0 +1,34 @@
+package Arrays.Sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    static void main() {
+     int arr[] = {3, 5, 6, 7 ,2, 6};
+      int res[] = bubbleSort(arr);
+        System.out.println(Arrays.toString(res));
+    }
+
+    static int[] bubbleSort(int a[])
+    {
+        for(int i=0; i<a.length-1; i++)
+        {
+            boolean isSwap = true;
+            for(int j=0; j<a.length-1-i; j++)
+            {
+
+                if(a[j] > a[j+1])
+                {
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                    isSwap = false;
+                }
+            }
+            if(isSwap) break;
+        }
+
+       return a;
+
+    }
+}
