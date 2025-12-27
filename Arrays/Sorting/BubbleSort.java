1725c2e0658f70b813e83e5151b463a569492d38
Alok Kumar Naik <aloknaik016261@gmail.com>
2025-12-27 08:53:34 +0530

    add

--- Arrays/Sorting/BubbleSort.java
+++ Arrays/Sorting/BubbleSort.java
@@ -6,7 +6,9 @@ public class BubbleSort {
     static void main() {
      int arr[] = {3, 5, 6, 7 ,2, 6};
       int res[] = bubbleSort(arr);
+      int res2[] = bubbleSortDesc(arr);
         System.out.println(Arrays.toString(res));
+        System.out.println(Arrays.toString(res2));
     }
 
     static int[] bubbleSort(int a[])
@@ -29,6 +31,29 @@ public class BubbleSort {
         }
 
        return a;
+
+    }
+
+    static int[] bubbleSortDesc(int a[])
+    {
+        for(int i=0; i<a.length-1; i++)
+        {
+            boolean isSwap = true;
+            for(int j=0; j<a.length-1-i; j++)
+            {
+
+                if(a[j] < a[j+1])
+                {
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                    isSwap = false;
+                }
+            }
+            if(isSwap) break;
+        }
+
+        return a;
 
     }
 }
