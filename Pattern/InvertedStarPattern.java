560bb610750d6bcc29cb2c8abe209d8d6a8383cf
Alok Kumar Naik <aloknaik016261@gmail.com>
2025-12-28 22:33:30 +0530

    update InvertedStarPattern

--- Pattern/InvertedStarPattern.java
+++ Pattern/InvertedStarPattern.java
@@ -1,14 +1,6 @@
 package Pattern;
 
 public class InvertedStarPattern {
-
-//    Enter no of Rows
-//    5
-//        * * * * * * * * *
-//          * * * * * * *
-//           * * * * *
-//             * * *
-//               *
     public static void main() {
         System.out.println("Enter no of Rows");
         int row = new java.util.Scanner(System.in).nextInt();
@@ -18,14 +10,13 @@ public class InvertedStarPattern {
     {
         int space = 0;
         int star = n*2-1;
-        for (int i = n; i >=1 ; i--) {
-            for (int j = space; j >=1 ; j--) {
+        for (int i = 0; i <=n ; i++) {
+            for (int j = 0; j <=space ; j++) {
                 System.out.print("  ");
             }
-            for (int k = star; k >= 1 ; k--) {
+            for (int k = 1; k <=star ; k++) {
                 System.out.print("* ");
             }
-
             System.out.println();
             space++;
             star-=2;
