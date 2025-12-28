261518406eb9ae760673410bedbb6f9b2df1c8f2
Alok Kumar Naik <aloknaik016261@gmail.com>
2025-12-28 21:08:29 +0530

    add rightAlignedOddNumberPyramid

---
+++ Pattern/RightAlignedOddNumberPyramid.java
@@ -0,0 +1,26 @@
+package Pattern;
+
+public class RightAlignedOddNumberPyramid {
+    public static void main() {
+        System.out.println("Enter no of row");
+        int n = new java.util.Scanner(System.in).nextInt();
+        rightAlignedOddNumberPyramid(n);
+    }
+
+    static void rightAlignedOddNumberPyramid(int n)
+    {
+        int space = n-1;
+        int star = 1;
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=space ; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <=star ; k++) {
+                System.out.print(star + " ");
+            }
+            System.out.println();
+            space--;
+            star+=2;
+        }
+    }
+}
